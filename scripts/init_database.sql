e4fcd5d62dc1a736a3dc224ce406adfef7a54254
nouhailaberghane <104172916+nouhailaberghane@users.noreply.github.com>
2025-05-11 23:26:45 +0100

    database & schemas

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -1,1 +1,38 @@
+/*
+=============================================================
+Ce script crée une base de données appelée 'DataWarehouse' si elle n'existe pas déjà.
+Ensuite, il ajoute trois schémas : 'bronze', 'silver' et 'gold', s'ils n'existent pas.
+=============================================================
+
+*/
+
+
+IF NOT EXISTS (SELECT name FROM sys.databases WHERE name = 'DataWarehouse')
+BEGIN
+    CREATE DATABASE DataWarehouse;
+END;
+GO
+
+-- Use the 'DataWarehouse' database
+USE DataWarehouse;
+GO
+
+-- Create Schemas if they do not already exist
+IF NOT EXISTS (SELECT * FROM sys.schemas WHERE name = 'bronze')
+BEGIN
+    EXEC('CREATE SCHEMA bronze');
+END;
+GO
+
+IF NOT EXISTS (SELECT * FROM sys.schemas WHERE name = 'silver')
+BEGIN
+    EXEC('CREATE SCHEMA silver');
+END;
+GO
+
+IF NOT EXISTS (SELECT * FROM sys.schemas WHERE name = 'gold')
+BEGIN
+    EXEC('CREATE SCHEMA gold');
+END;
+GO
 
